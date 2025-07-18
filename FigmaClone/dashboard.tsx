f776438c460d481e4ca184f75d72f21d4a5a8023
Niranjan Koirala <121800695+Neranzann@users.noreply.github.com>
2025-07-19 00:04:17 +0545

    Update dashboard.tsx

--- FigmaClone/dashboard.tsx
+++ FigmaClone/dashboard.tsx
@@ -1,1 +1,19 @@
+"use client"
+import Sidebar from "./sidebar"
+import ClientsTable from "./clients-table"
+import Header from "./header"
+
+export default function Dashboard() {
+  return (
+    <div className="flex h-screen w-full bg-[#f8f8f8]">
+      <Sidebar />
+      <div className="flex flex-col flex-1 overflow-hidden">
+        <Header />
+        <main className="flex-1 overflow-auto p-6">
+          <ClientsTable />
+        </main>
+      </div>
+    </div>
+  )
+}
 
